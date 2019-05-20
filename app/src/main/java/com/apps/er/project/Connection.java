76ee6f11fef241f59ca5d13bb5507599fbea63d4
EdgardRivas <edgard.rivas@hotmail.com>
2019-05-19 22:51:07 -0600

    Changed to public login & getPoints strings

--- app/src/main/java/com/apps/er/project/Connection.java
+++ app/src/main/java/com/apps/er/project/Connection.java
@@ -2,8 +2,8 @@ package com.apps.er.project;
 
 public class Connection {
     private static final String IP = "http://192.168.0.5:8080/finder/functions/";
-    private static final String login = IP + "login.php";
-    private static final String getPoints = IP + "getPoints.php";
+    public static final String login = IP + "login.php";
+    public static final String getPoints = IP + "getPoints.php";
     public static final String getWorkshops = IP + "getWorkshops.php";
     public static final String getProviders = IP + "getProviders.php";
     public static final String getProducts = IP + "/getProducts.php";
